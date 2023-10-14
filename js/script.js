0f66b059b9525c92567b0abee1c75534dc23482b
FC0ELHO <facp84@gmail.com>
2023-10-13 22:43:48 -0300

    Ajuste para que se peuda ver la API de Pokemon

--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
-//Llamado de API
-function callDatos (information) {
-    const respuesta = fetch('https://api.thecatapi.com/v1/images/search?limit=10');
-
-    respuesta
-    .then( resInfo => resInfo.json())
-    .then(datos => {
-        information(datos)
-    });    
+async function buscarPet(id) {
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`) ;//hago mi solicitud a la web que tiene la api       
+    return await respuesta.json(); // el resultado dentro de la funcoin la manejo y la convierto en json
 }
-callDatos( datos => {
-    const limite = 4;
-    let contador = 1
-    datos.forEach(resInfo => {
-        if (contador <= limite){
-        const articleImg = document.createRange().createContextualFragment(/*con esta funcionalidad puedo agregar codigo html dentro de estos apostrofes `  */`
-        <article class="article_img">
-            <div class="contenedor_img">
-                <img src="${resInfo.url}" alt="">
-            </div>                
-                <h2>${resInfo.origin}</h2>
-        </article>
+
+async function buscarLasPets() { //le coloco async para indicar que tendre await dentro, como quien dice para que espere que se ejecute lo que esta dentro
+    const limite = 09;              //es decir para que espere la promesa (que seria una respuesta obligatoria)
+    const marco = document.querySelector(".marco");
+
+    for (let i = 6; i <= limite; i++) {
+        const datos = await buscarPet(i);
+        const articleImg = document.createRange().createContextualFragment(`
+            <article class="article_img">
+                <div class="contenedor_img">
+                    <img src="${datos.sprites.front_default}" alt="">
+                </div>
+                <h2>${datos.name}</h2>
+            </article>
         `);
-        const marco = document.querySelector(".marco");
         marco.appendChild(articleImg);
-        contador++;
-        }
-        else {
-            return;
-        }
-    });
-});
+    }
+}
+
+buscarLasPets();
